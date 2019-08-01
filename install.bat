e149029045bc9473496ccdfd26b77fc788588d17
Stewart Smith <stewart.dryden.smith@gmail.com>
2019-08-01 16:35:32 -0700

    Windows installer

---
+++ install.bat
@@ -0,0 +1,15 @@
+@echo off
+SETLOCAL
+
+IF DEFINED HOME (
+    SET dest=%HOME%
+)ELSE (
+    SET dest=%UserProfile%
+)
+
+mklink %dest%\.bashrc	 %~dp0.bashrc
+mklink %dest%\.emacs	 %~dp0.emacs
+mklink %dest%\.gdbinit	 %~dp0.gdbinit
+mklink %dest%\.tmux.conf %~dp0.tmux.conf
+mklink %dest%\.vimrc	 %~dp0.vimrc
+mklink %dest%\.zshrc	 %~dp0.zshrc
