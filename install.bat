abe706aa654a561f61432516294e0d3a054bbb53
Stewart Smith <stewart.dryden.smith@gmail.com>
2019-08-01 17:39:25 -0700

    Update with custom.el

--- install.bat
+++ install.bat
@@ -8,7 +8,10 @@ IF DEFINED HOME (
 )
 
 mklink %dest%\.bashrc	 %~dp0.bashrc
-mklink %dest%\.emacs	 %~dp0.emacs
+
+mklink %dest%\.emacs	         %~dp0.emacs
+mklink %dest%\.emacs.d\custom.el %~dp0.emacs.d\custom.el
+
 mklink %dest%\.gdbinit	 %~dp0.gdbinit
 mklink %dest%\.tmux.conf %~dp0.tmux.conf
 mklink %dest%\.vimrc	 %~dp0.vimrc
