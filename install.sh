5e5e3f676631dc02ad7ad1e2fee7dfe92db330f0
Stewart Smith <stewart.dryden.smith@gmail.com>
2019-09-03 22:59:34 -0700

    Create emacs dir automatically

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ln -s $(readlink -f .bashrc)    ~/.bashrc
 
 ln -s $(readlink -f .emacs)             ~/.emacs
+mkdir -p ~/.emacs.d
 ln -s $(readlink -f .emacs.d/custom.el) ~/.emacs.d/custom.el
 
 ln -s $(readlink -f .gdbinit)   ~/.gdbinit
