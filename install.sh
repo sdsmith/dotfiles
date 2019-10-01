ef27f72c83911b0202b070dfb73a10182ca3456a
Stewart Smith <stewarts@nvidia.com>
2019-10-01 11:26:05 -0700

    Add VNC config

--- install.sh
+++ install.sh
@@ -8,4 +8,5 @@ ln -s $(readlink -f .emacs.d/custom.el) ~/.emacs.d/custom.el
 ln -s $(readlink -f .gdbinit)   ~/.gdbinit
 ln -s $(readlink -f .tmux.conf) ~/.tmux.conf
 ln -s $(readlink -f .vimrc)     ~/.vimrc
+ln -s $(readlink -f .vnc)       ~/.vnc
 ln -s $(readlink -f .zshrc)     ~/.zshrc
