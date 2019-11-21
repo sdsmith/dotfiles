a3733d6c3fa9baf4bb80c4d35f50dd0289cce5cf
Stewart Smith <stewart.dryden.smith@gmail.com>
2019-11-21 15:01:25 -0800

    Fix windows link names

--- install.bat
+++ install.bat
@@ -7,12 +7,12 @@ IF DEFINED HOME (
     SET dest=%UserProfile%
 )
 
-mklink %dest%\.bashrc	 %~dp0.bashrc
+mklink "%dest%\.bashrc"	"%~dp0.bashrc"
 
-mklink %dest%\.emacs	         %~dp0.emacs
-mklink %dest%\.emacs.d\custom.el %~dp0.emacs.d\custom.el
+mklink "%dest%\.emacs"	           "%~dp0.emacs"
+mklink "%dest%\.emacs.d\custom.el" "%~dp0.emacs.d\custom.el"
 
-mklink %dest%\.gdbinit	 %~dp0.gdbinit
-mklink %dest%\.tmux.conf %~dp0.tmux.conf
-mklink %dest%\.vimrc	 %~dp0.vimrc
-mklink %dest%\.zshrc	 %~dp0.zshrc
+mklink "%dest%\.gdbinit"   "%~dp0.gdbinit"
+mklink "%dest%\.tmux.conf" "%~dp0.tmux.conf"
+mklink "%dest%\.vimrc"	   "%~dp0.vimrc"
+mklink "%dest%\.zshrc"	   "%~dp0.zshrc"
