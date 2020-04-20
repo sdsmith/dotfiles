81f76c70d3cfaa33583372b0d554a244cf9d0278
Stewart Smith <stewart.dryden.smith@gmail.com>
2020-04-20 00:49:57 -0700

    Require bash for readlink

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 CMD_READLINK="readlink"
 if uname -a | grep -qE "Darwin" &> /dev/null ; then
     CMD_READLINK="greadlink"
 fi
     
-ln -s $($CMD_READLINK -f .bashrc)    ~/.bashrc
+ln -s $($CMD_READLINK -f .bashrc)    $HOME/.bashrc
 
-ln -s $($CMD_READLINK -f .emacs)     ~/.emacs
-mkdir -p ~/.emacs.d
-ln -s $($CMD_READLINK -f .emacs.d/custom.el) ~/.emacs.d/custom.el
+ln -s $($CMD_READLINK -f .emacs)     $HOME/.emacs
+mkdir -p $HOME/.emacs.d
+ln -s $($CMD_READLINK -f .emacs.d/custom.el) $HOME/.emacs.d/custom.el
 
-ln -s $($CMD_READLINK -f .gdbinit)   ~/.gdbinit
-ln -s $($CMD_READLINK -f .tmux.conf) ~/.tmux.conf
-ln -s $($CMD_READLINK -f .vimrc)     ~/.vimrc
-ln -s $($CMD_READLINK -f .vnc)       ~/.vnc
-ln -s $($CMD_READLINK -f .zshrc)     ~/.zshrc
+ln -s $($CMD_READLINK -f .gdbinit)   $HOME/.gdbinit
+ln -s $($CMD_READLINK -f .tmux.conf) $HOME/.tmux.conf
+ln -s $($CMD_READLINK -f .vimrc)     $HOME/.vimrc
+ln -s $($CMD_READLINK -f .vnc)       $HOME/.vnc
+ln -s $($CMD_READLINK -f .zshrc)     $HOME/.zshrc
