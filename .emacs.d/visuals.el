e8244196298270666f9e7b174284b3c95cb8a6a0
Stewart Smith <stewart.dryden.smith@gmail.com>
2020-07-25 23:44:27 -0400

    Add hex color codes

--- .emacs.d/visuals.el
+++ .emacs.d/visuals.el
@@ -1,22 +1,23 @@
 ;; Set color scheme
 (add-to-list 'default-frame-alist '(font . "Liberation Mono-9.5"))
-(set-face-attribute 'font-lock-builtin-face nil :foreground "#DAB98F")
-(set-face-attribute 'font-lock-comment-face nil :foreground "gray60")
-(set-face-attribute 'font-lock-constant-face nil :foreground "DarkKhaki")
-(set-face-attribute 'font-lock-doc-face nil :foreground "burlywood3")
-(set-face-attribute 'font-lock-function-name-face nil :foreground "IndianRed1")
-(set-face-attribute 'font-lock-keyword-face nil :foreground "DarkGoldenrod3")
-(set-face-attribute 'font-lock-string-face nil :foreground "LightYellow1")
-(set-face-attribute 'font-lock-type-face nil :foreground "IndianRed1")
-(set-face-attribute 'font-lock-variable-name-face nil :foreground "burlywood3")
-(set-face-attribute 'font-lock-preprocessor-face nil :foreground "MediumPurple1")
-(set-face-attribute 'default nil :foreground "burlywood3")
+(set-face-attribute 'font-lock-builtin-face nil :foreground "burlywood3")
+(set-face-attribute 'font-lock-comment-face nil :foreground "gray60") ; #999999
+(set-face-attribute 'font-lock-constant-face nil :foreground "DarkKhaki") ; #bdb76b
+(set-face-attribute 'font-lock-doc-face nil :foreground "burlywood3") ; #cdaa7d
+(set-face-attribute 'font-lock-function-name-face nil :foreground "IndianRed1") ; #ff6a6a
+(set-face-attribute 'font-lock-keyword-face nil :foreground "DarkGoldenrod3") ; #cd950c
+;;(set-face-attribute 'font-lock-string-face nil :foreground "LightYellow1") ; #ffffe0
+(set-face-attribute 'font-lock-string-face nil :foreground "#ddddb6")
+(set-face-attribute 'font-lock-type-face nil :foreground "IndianRed1") ; #ff6a6a
+(set-face-attribute 'font-lock-variable-name-face nil :foreground "burlywood3") ; #cdaa7d
+(set-face-attribute 'font-lock-preprocessor-face nil :foreground "MediumPurple1") ; #ab82ff
+(set-face-attribute 'default nil :foreground "burlywood3") ; #cdaa7d
 
-(add-to-list 'default-frame-alist '(foreground-color . "burlywood3"))
+(add-to-list 'default-frame-alist '(foreground-color . "burlywood3")) ; #cdaa7d
 (add-to-list 'default-frame-alist '(background-color . "#161616"))
 (add-to-list 'default-frame-alist '(cursor-color     . "#40FF40"))
 
-(set-face-attribute 'minibuffer-prompt nil :foreground "cyan")
+(set-face-attribute 'minibuffer-prompt nil :foreground "cyan") ; #00ffff
 
 ;; Highlight current line
 (global-hl-line-mode 1)
@@ -26,5 +27,5 @@
                      ;; "#8b1a1a" ; deep red
                      "#8b0000")
 
-(set-face-background 'isearch-fail "red")
-(set-face-background 'region "blue")
+(set-face-background 'isearch-fail "red") ; #ff0000
+(set-face-background 'region "blue") ; #0000ff
